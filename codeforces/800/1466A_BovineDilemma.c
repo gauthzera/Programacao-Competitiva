d4d76f4f0f06e7192e5d4ae6b4ebbd18a28739b9
Gabriel Ulhoa <gabrielulhoa@outlook.com>
2026-03-27 15:03:37 -0300

    melhor resolução 1466A CF

--- codeforces/800/1466A_BovineDilemma.c
+++ codeforces/800/1466A_BovineDilemma.c
@@ -1,41 +1,34 @@
 #include <stdio.h>
-
-int main(){
+    
+int main() {
     int t;
     scanf("%d",&t);
 
     while(t--){
         int n;
-        scanf("%d",&n);
-        int xs[n];
-        for(int i=0;i<n;i++){
-            scanf("%d",&xs[i]);
-        }
+        scanf("%d",&n); //num trees
+        int arr[n];
         
-        int qTri=n*(n-1)/2;//quantidade de triangulos
-        int arrT[qTri]; //array com a quantidade de todas as bases;
+        for(int i=0;i<n;i++){
+            scanf("%d",&arr[i]);
+        }
+       
+        int bases[51]={0}; //marca 1 para todas as possíveis bases
 
-        int ind=0;
-        for(int i=1;i<n;i++){
-            int x=xs[i];
+        for(int i=n-1;i>0;i--){
+            int arvore=arr[i];
             for(int j=i-1;j>=0;j--){
-                int base=x-xs[j];
-                arrT[ind++]=base; //adiciono todas as possíveis bases no array
+                int base=arvore-arr[j]; //calcula uma base possível
+                bases[base]=1;
             }
         }
-
-        int bases[51]={0}; // possiveis bases= 1 até 50;
-        for(int i=0;i<qTri;i++){
-               bases[arrT[i]]++; //marca a quantidade de vezes que aparece um triângulo a base arrT[i]
+        int q=0;
+        for(int i=1;i<51;i++){
+            if(bases[i]==1)q++;
         }
+        printf("%d\n",q);
 
-        int total=0;
-        for(int i=1;i<=50;i++){
-                if(bases[i]!=0)total++; //quantidade de bases diferentes
-        }
-        printf("%d\n",total);
-        
     }
     
-	return 0;
+    return 0;
 }
