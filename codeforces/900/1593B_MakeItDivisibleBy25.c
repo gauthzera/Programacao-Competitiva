d440aee3dd7d25da21c5dfd0355fa2b5594abf61
Gabriel Ulhoa <gabrielulhoa@outlook.com>
2026-05-29 18:10:03 -0300

    1593B -900 CF

---
+++ codeforces/900/1593B_MakeItDivisibleBy25.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+#define ll long long
+
+int custo(char c[],char n1,char n2){
+    int q=0;
+    int tam=strlen(c);
+
+    int indx=tam-1;
+
+    while(indx>=0 && c[indx]!=n2){
+        q++;
+        indx--;
+    }
+
+    if(indx<0)return 100; //não encontrou
+
+    indx--;
+    while(indx>=0 && c[indx]!=n1){
+        q++;
+        indx--;
+    }
+
+    if(indx<0)return 100; //não encontrou
+    
+    return q; //quantidade de removidos
+}
+
+
+int main(){
+    int t;
+    scanf("%d",&t);
+
+    while(t--){
+        char num[20];
+        scanf("%s",num);
+        
+
+        int c1=custo(num,'0','0');
+        int c2=custo(num,'2','5');
+        int c3=custo(num,'5','0');
+        int c4=custo(num,'7','5');
+        
+        int menor=c1;
+        if(c2<menor)menor=c2;
+        if(c3<menor)menor=c3;
+        if(c4<menor)menor=c4;
+
+        printf("%d\n",menor);
+    }
+    
+    return 0;
+}
