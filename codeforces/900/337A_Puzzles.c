639d090136285d483330f7093b1dd6cf2ee0909e
Gabriel Ulhoa <gabrielulhoa@outlook.com>
2026-06-07 01:57:01 -0300

    337A CF

--- codeforces/900/337A_Puzzles.c
+++ codeforces/900/337A_Puzzles.c
@@ -1,56 +1,36 @@
 #include <stdio.h>
-    
-void quicksort(int arr[],int l,int r){
-    int pivot=arr[(l+r)/2];
-    int i=l,j=r;
-    
-    while(i<=j){
-        while(arr[i]<pivot)i++;
-        while(arr[j]>pivot)j--;
+#include <stdlib.h>
 
-        if(i<=j){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-            i++;
-            j--;
-        }
-    }
-    
-    if(i<r)quicksort(arr,i,r);
-    if(j>l)quicksort(arr,l,j);
+int cmp(const void *a,const void *b){
+    int x = *(int*)a;
+    int y = *(int*)b;
+ 
+    if (x < y) return -1;
+    if (x > y) return 1;
+    return 0;
 }
 
-
-int main() {
+int main(){ 
     int n,m;
     scanf("%d %d",&n,&m);
-    int arr[m];
 
+    int arr[m];
     for(int i=0;i<m;i++){
         scanf("%d",&arr[i]);
     }
-    
-    int pos=0;
-    int dif=1000; //valor maximo de peças
-    
-    quicksort(arr,0,m-1); //ordenação
 
-    while(n<=m){
-        int maior=arr[pos],menor=maior;
+    qsort(arr,m,sizeof(int),cmp);
 
-        for(int i=pos;i<n;i++){ 
-            if(arr[i]>maior)maior=arr[i];
-            if(arr[i]<menor)menor=arr[i];
-        }
-        //pega a diferença entre o maior e menor a cada n elementos
-        int aux=maior-menor;
-        if(aux<dif)dif=aux;
-        pos++;
-        n++;
+    int q=arr[m-1];
+    
+    int l=0;
+    for(int i=n-1;i<m;i++){
+        int dif=arr[i]-arr[l];
+        if(dif<q)q=dif;
+        l++;
     }
-
-    printf("%d\n",dif);
     
+    printf("%d\n",q);
+
     return 0;
 }
