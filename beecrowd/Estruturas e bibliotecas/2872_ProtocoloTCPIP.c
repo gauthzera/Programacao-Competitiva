fe8cda1aeec2ddcf82372ae730dc0ff3422a5d70
Gabriel Ulhoa <gabrielulhoa@outlook.com>
2026-06-11 17:15:46 -0300

    2872 BEECROWD

--- beecrowd/Estruturas e bibliotecas/2872_ProtocoloTCPIP.c
+++ beecrowd/Estruturas e bibliotecas/2872_ProtocoloTCPIP.c
@@ -1,77 +1,39 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-typedef struct Pacote{
-    char num[4];
-   struct Pacote* prox;
-}Pacote;
-
-Pacote* criaPacote(char s[]){
-    Pacote* p=malloc(sizeof(Pacote));
-    strcpy(p->num,s);
-    p->prox=NULL;
-    return p;
-}
-
-typedef struct Lista{
-    Pacote* primeiro;
-}Lista;
-
-Lista* criaLista(){
-    Lista* l=malloc(sizeof(Lista));
-    l->primeiro=NULL;
-    return l;
-}
-
-void inserir(Lista* lista,char s[]){
-    Pacote* novo=criaPacote(s);
-    novo->prox=lista->primeiro;
-    lista->primeiro=novo;
-}
-
-void mostrar(Lista* l){
-        Pacote* aux=l->primeiro;
-        while(aux!=NULL){
-            printf("Package %s\n",aux->num);
-            aux=aux->prox;
+void insertionSort(int arr[],int n){
+    for(int i=1;i<n;i++){
+        int key=arr[i];
+        int j=i-1;
+        
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
         }
-}
-
-void ordenaLista(Lista* l){ //selectionSort
-    for(Pacote* i=l->primeiro;i!=NULL;i=i->prox){
-        Pacote* min=i;
-
-        for(Pacote* j=i->prox;j!=NULL;j=j->prox){
-            if(strcmp(j->num,min->num)<0){
-                min=j;
-            }
-        }
-        char tmp[4]; //troca de num
-        strcpy(tmp,i->num);
-        strcpy(i->num,min->num);
-        strcpy(min->num,tmp);
-
+        arr[j+1]=key;
     }
 }
 
 int main(){
-    int op;
+    int comando;
     
-    while(scanf("%d",&op)!=EOF){
-        Lista* l=criaLista();
-        char num[4],p[8];
-        scanf("%s",p); //lê o package
-
+    while(scanf("%d",&comando)!=EOF){
+        char p[20];
+        int num,qtd=0;
+        int arr[1000];
+        
+        scanf("%s",p);
         while(p[0]!='0'){
-            scanf("%s",num);
-            inserir(l,num);
+            scanf("%d",&arr[qtd++]);
             scanf("%s",p);
         }
-        ordenaLista(l);
-        mostrar(l);
+
+        insertionSort(arr,qtd);
+        
+        for(int i=0;i<qtd;i++){
+            printf("Package %.03d\n",arr[i]);
+        }
         printf("\n");
     }
-    
-    return 0;
+
+    return 0; 
 }
